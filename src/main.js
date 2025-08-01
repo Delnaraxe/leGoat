c92f4657012f672a8daa3bcfa434f34d710847b3
Delnaraxe <draymore23@gmail.com>
2025-08-02 01:13:26 +0200

    + placer position origine joueur

--- src/main.js
+++ src/main.js
@@ -1,12 +1,38 @@
-// cloneNode(false)
-//add A1-7 to div
 const chessboard = document.getElementById("chessboard");
 const newTile = document.createElement("div");
 
-function createDiv() {
-  chessboard.appendChild(newTile.cloneNode(false))
+function initChessboard() {
+  for (let i = 1; i <= 49; i++) {
+    const tile = newTile.cloneNode(false);
+    tile.setAttribute("data-pos", i);
+    chessboard.appendChild(tile);
+  }
 }
+initChessboard();
 
-for (let i = 1; i < 49; i++) {
-  createDiv();
+const coordinatesMap = {};
+function initCoordinatesMap() {
+  for (let i = 1; i < 8; i++) {
+    const letter = String.fromCharCode(64 + i); // 65 = 'A' en ASCII
+    coordinatesMap[letter] = [];
+    for (let j = i; j <= 49; j = j + 7) {
+      coordinatesMap[letter].push(j);
+    }
+  }
+  console.log(coordinatesMap);
 }
+initCoordinatesMap();
+
+function initHorsePos() {
+  const initPosition = ["D", "4"];
+  const col = initPosition[0];
+  const row = initPosition[1];
+  const position = coordinatesMap[col][row - 1];
+
+  const horseIcon = document.createElement("i");
+  horseIcon.classList.add("ph", "ph-horse");
+
+  const initTileDiv = document.querySelector(`[data-pos="${position}"]`);
+  initTileDiv.appendChild(horseIcon);
+}
+initHorsePos();
